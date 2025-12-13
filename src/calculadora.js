77903ba6565523cae1ea4975d7da0834bdf13f27
Iago Santini <iagosantini@gmail.com>
2025-12-13 13:36:23 -0300

    Adicionei a funcao de soma e seu teste

---
+++ src/calculadora.js
@@ -0,0 +1,17 @@
+function somarDoisNumeros(valor1, valor2) {
+    const resultado = valor1 + valor2;
+    return resultado;
+}
+
+function calcularMediaDeDoisNumeros(valor1, valor2){
+    // 1. soma todos os valores
+    const resultadoDaSoma = somarDoisNumeros(valor1, valor2)
+    // 2. dividir pela quantidade de numeros passados
+    const mediaDeDoisValores = resultadoDaSoma / 2;
+    // 3. exibe o resultado
+    return mediaDeDoisValores;
+}
+
+module.exports = {
+    somarDoisNumeros
+}
---
+++ tests/calculadora.test.js
@@ -0,0 +1,4 @@
+const { somarDoisNumeros } = require('../src/calculadora.js')
+
+const resultadoDaSoma = somarDoisNumeros(5, 3)
+console.log(resultadoDaSoma)
